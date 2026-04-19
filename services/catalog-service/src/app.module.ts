e7b50c2a3a159020d34f1612073cc6bcb1e36d49
Victor Fernandes Martins <42803987+vchopDev@users.noreply.github.com>
2026-04-19 16:36:50 +0200

    feat: scaffold catalog-service with NestJS, Fastify, and MongoDB

---
+++ services/catalog-service/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+
+@Module({
+  imports: [ProductsModule, CategoriesModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
